45a0e49bacc2223dcad9aa613dd4b2d1f3e8dbac
Francheska Rivano <frivano@beautytrak.com>
2023-12-05 15:36:40 +0800

    Decrypt plainText value

---
+++ index.js
@@ -0,0 +1,17 @@
+function decrypt(cipherText, key) {
+    const reverseKey = Object.fromEntries([...key].map((char, index) => [char, String.fromCharCode(index + 65)]));
+
+    let plainText = '';
+    for (let i = 0; i < cipherText.length; i++) {
+        plainText += reverseKey[cipherText[i]];
+    }
+
+    return plainText;
+}
+
+const cipherText = 'FZGGh43F5V';
+const key = '7LwU3Fp4Z6PtyG5bzX5h7PhVQa';
+
+const plainText = decrypt(cipherText, key);
+
+console.log(plainText);
